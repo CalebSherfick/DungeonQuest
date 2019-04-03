b6c73027bb1a4d466bc56d10b44b076885206afc
Caleb Sherfick <calebsherfick@gmail.com>
2019-04-03 14:41:07 -0600

    set up Room model

--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CastleGrimtol.Project.Interfaces;
 
@@ -7,7 +8,53 @@ namespace CastleGrimtol.Project.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
-    public List<Item> Items { get; set; }
-    public Dictionary<string, IRoom> Exits { get; set; }
+    List<Item> Items { get; set; }
+    Dictionary<Direction, IRoom> Exits { get; set; }
+
+
+    public void PrintRoomItems()
+    {
+      Console.WriteLine("Interactable items in the room: ");
+      Items.ForEach(item =>
+      {
+        Console.WriteLine(item.Name);
+      });
+    }
+
+    public void AddExit(Direction direction, IRoom room)
+    {
+      Exits.Add(direction, room);
+    }
+
+    public IRoom TraveltoRoom(Direction dir)
+    {
+      if (Exits.ContainsKey(dir))
+      {
+        return Exits[dir];
+      }
+      System.Console.WriteLine("There is nowhere to go in that direction.");
+      return (IRoom)this;
+    }
+
+    public Room(string name, string desc)
+    {
+      Exits = new Dictionary<Direction, IRoom>();
+      Items = new List<Item>();
+      Name = name;
+      Description = desc;
+    }
   }
+
+  public enum Direction
+  {
+    north,
+    east,
+    south,
+    west
+  }
+
+
+
+
+
 }
